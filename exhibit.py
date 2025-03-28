7807303a852a8467f77fc938d16430b8f23b7521
WangEntang <maxewang10@gmail.com>
2025-03-28 18:57:18 +0100

    3.28 modify exhibit

--- exhibit.py
+++ exhibit.py
@@ -26,13 +26,12 @@ model.to(device)
 model.eval()
 tokenizer = AutoTokenizer.from_pretrained('roberta-base', use_fast=True, add_prefix_space=True)
 
-num_examples_to_show = 5
+num_examples_to_show = 20
 shown = 0
 
 # 遍历原始数据集，先对原始文本使用 tokenizer 得到 input_ids，然后送入模型预测（注意CRF情况）
 for example in raw_test_dataset:
     tokens = example['tokens']  # 原始单词列表
-    true_tags = [label_list[tag] for tag in example['ner_tags']]  # 原始标注
 
     # 使用 fast tokenizer 进行 tokenization，并保持原始单词与tokenize后结果的对应（例如使用 is_split_into_words=True）
     encoding = tokenizer(tokens, is_split_into_words=True, return_offsets_mapping=True, truncation=True)
@@ -48,7 +47,7 @@ for example in raw_test_dataset:
         outputs = model(input_ids=input_ids_tensor, attention_mask=attention_mask)
     # 如果使用 CRF，outputs 为预测的标签序列（列表形式）；否则为 logits，需要进行 argmax
     if is_CRF:
-        pred_tags_ids = outputs  # 取出预测的序列（假设返回的是 list of list）
+        pred_tags_ids = outputs[0]  # 取出预测的序列（假设返回的是 list of list）
     else:
         logits = outputs
         pred_tags_ids = torch.argmax(logits, dim=2)[0]
@@ -62,19 +61,18 @@ for example in raw_test_dataset:
     previous_word_idx = None
 
     print(f"Example {shown+1}") 
-    print(f"{'Word':<15}{'NER tag':<15}")
+    print(f"{'Word':<15}{'NER tag':<15}{'Label':<15}")
 
     for idx, word_idx in enumerate(word_ids):
         if word_idx is None:
             continue
         # 仅在当前 token 对应的是一个新单词时取标签
         if word_idx != previous_word_idx:
-            pred_tags.append(label_list[pred_tags_ids[idx]])
             previous_word_idx = word_idx
             word = example["tokens"][word_idx]
             NER_tag = label_list[pred_tags_ids[idx]]
-        
-            print(f"{word:<15}{NER_tag:<15}")
+            label = label_list[example["ner_tags"][word_idx] ]
+            print(f"{word:<15}{NER_tag:<15}{label:<15}")
     
     shown += 1
     if shown >= num_examples_to_show:
